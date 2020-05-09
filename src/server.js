a6ae6cbc18ccd8d374a0701c0e82eed3bb5498db
Milan Cvetic <m.cvetic@vegaitsourcing.rs>
2020-05-09 12:37:03 +0200

    Changed exposed port.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const PORT = 8787;
+const PORT = 8888;
 const HOST = '0.0.0.0';
 const MONGO_URL = 'mongodb://mongo/devops-workshop-express';// Url to the mongo container and test-express database.
 
