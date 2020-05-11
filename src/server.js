67deaa8be7802a86e43b29249ceb2e0c618deff4
Milan Cvetic <m.cvetic@vegaitsourcing.rs>
2020-05-11 10:54:35 +0200

    Fix.

--- src/server.js
+++ src/server.js
@@ -5,9 +5,10 @@ const PORT = 9000;
 const HOST = '0.0.0.0';
 const MONGO_URL = 'mongodb://mongo/devops-workshop-express';// Url to the mongo container and test-express database.
 
-const app = express();
 mongoose.connect(MONGO_URL, { useNewUrlParser: true })
     .then(() => {
+        const app = express();
+
         app.get('/', (request, response) => response.send('Hello World!'));
 
         app.listen(PORT, HOST);
